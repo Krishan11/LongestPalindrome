8639a1829375fa50a111b2258b94e4801491e9ab
misterX5 <shupx11@gmail.com>
2016-02-19 17:22:48 -0600

    Update LongestPalindrome.java
    deletes Life.painInside variable

--- src/LongestPalindrome.java
+++ src/LongestPalindrome.java
@@ -1,36 +1,36 @@
-
-
-public class LongestPalindrome
-{
-	public static String findLongestPalindrome(String str)
-	{
-		String currentPalindrome="",longestPalindrome="";
-		for(int i=1;i<str.length()-1;i++)
-		{int left=i-1,right=i+1;
-			while(left >=0 && right<str.length())
-			{
-				if(str.charAt(left)!=str.charAt(right))
-				{
-				break;
-				}
-			currentPalindrome=str.substring(left,right+1);
-			if(currentPalindrome.length()>longestPalindrome.length())
-			{
-				longestPalindrome=currentPalindrome;
-			}
-			left--;
-			right++;
-			
-			}
-				
-		}
-	return longestPalindrome;
-		
-	}
-	public static void main (String args[])
-	{
-		String str="cuoyoucffffggggffff";
-		System.out.println(findLongestPalindrome(str));
-		
-	}
-}
+// Swag Head Master
+// swag
+public class LongestPalindrome // TODO: Realize that Bush did 9/11
+{ // swag
+	public static String findLongestPalindrome(String str) // swag
+	{ // swag
+		String currentPalindrome="",longestPalindrome=""; // swag
+		for(int i=1;i<str.length()-1;i++) // swag
+		{int left=i-1,right=i+1; // swag
+			while(left >=0 && right<str.length()) // swag
+			{ // swag
+				if(str.charAt(left)!=str.charAt(right)) // swag
+				{ // swag
+				break; // swag
+				} // swag
+			currentPalindrome=str.substring(left,right+1); // swag
+			if(currentPalindrome.length()>longestPalindrome.length()) // swag
+			{ // swag
+				longestPalindrome=currentPalindrome; // swag
+			} // This is where Waldo really lives. What you see in the books is a scam.
+			left--;  // swag
+			right++; // swag
+			 // swag
+			} // swag
+			// swag	
+		} // swag
+	return longestPalindrome; // swag
+	 // swag	
+	} // swag
+	public static void main (String args[]) // swag
+	{ // swag
+		String str="cuoyoucffffggggffff"; // swag
+		System.out.println(findLongestPalindrome(str)); // swag
+	// swag	
+	} // swag
+} // swag
